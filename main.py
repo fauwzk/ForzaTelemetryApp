cdf38cfc9f57427f8c34ebd2cafaedf7c8c19911
Fauwzk <polentesaxel@gmail.com>
2022-03-22 18:41:47 +0100

    Auto stash before rebase of "origin/main"

--- main.py
+++ main.py
@@ -24,10 +24,28 @@ with dpg.value_registry():
 	dpg.add_string_value(default_value=gear_setting_default, tag="gear_setting")
 	dpg.add_int_value(default_value=int(gear_setting_default), tag="gearbox")
 
+texture_not_running = []
+for i in range(0, 100 * 100):
+	texture_not_running.append(255 / 255)
+	texture_not_running.append(0)
+	texture_not_running.append(0)
+	texture_not_running.append(255 / 255)
+
+texture_running = []
+for i in range(0, 100 * 100):
+	texture_running.append(0)
+	texture_running.append(255 / 255)
+	texture_running.append(0)
+	texture_running.append(255 / 255)
+
+with dpg.texture_registry(show=False):
+	dpg.add_static_texture(100, 100, texture_not_running, tag="not_running")
+	dpg.add_static_texture(100, 100, texture_running, tag="running")
+
 def connect():
 	ip = dpg.get_value("ip_address")
 	port = dpg.get_value("port")
-	#print(ip, port)
+	# print(ip, port)
 	try:
 		data_gen.set_server(ip, port)
 		dpg.set_value("connect_status", "Connected")
@@ -40,13 +58,13 @@ def run():
 	dpg.set_value("run_status", "Running")
 	while True:
 		try:
-			data, addr = data_gen.sock.recvfrom(1500) # buffer size is 1500 bytes, this line reads data from the socket
+			data, addr = data_gen.sock.recvfrom(1500)  # buffer size is 1500 bytes, this line reads data from the socket
 			returned_data = data_gen.get_data(data)
-			gear = returned_data['Gear']
-			rpm = round(returned_data['CurrentEngineRpm'])
-			power = round((returned_data['Power']*1.34102)/1000)
-			torque = round(returned_data['Torque'], 1) 
-			boost = round(returned_data['Boost']/14.504, 2)
+			gear = returned_data["Gear"]
+			rpm = round(returned_data["CurrentEngineRpm"])
+			power = round((returned_data["Power"] * 1.34102) / 1000)
+			torque = round(returned_data["Torque"], 1)
+			boost = round(returned_data["Boost"] / 14.504, 2)
 			dpg.set_value("gear", gear)
 			dpg.set_value("rpm", rpm)
 			dpg.set_value("boost", boost)
@@ -56,12 +74,13 @@ def run():
 			else:
 				dpg.set_value("power", 0)
 				dpg.set_value("torque", 0)
-				continue		
-			#print(f"RPM = {rpm}, Power = {power}, Torque = {torque}, Boost = {boost}")
+				continue
+			# print(f"RPM = {rpm}, Power = {power}, Torque = {torque}, Boost = {boost}")
 		except:
 			dpg.set_value("run_status", "Error")
 
 def graph():
+	dpg.set_value("graph_status", "Preparing")
 	global rpm_axis
 	global power_axis
 	global torque_axis
@@ -71,20 +90,18 @@ def graph():
 	torque_axis = []
 	boost_axis = []
 	dpg.set_value("graph_status", "Running")
-	gear_setting = int(dpg.get_value("gearbox")) 
+	gear_setting = int(dpg.get_value("gearbox"))
 	dpg.set_value("gear_setting", str(gear_setting))
 	try:
-		data, addr = data_gen.sock.recvfrom(1500) # buffer size is 1500 bytes, this line reads data from the socket
-		returned_data = data_gen.get_data(data)
 		while True:
-			data, addr = data_gen.sock.recvfrom(1500) # buffer size is 1500 bytes, this line reads data from the socket
+			data, addr = data_gen.sock.recvfrom(1500)  # buffer size is 1500 bytes, this line reads data from the socket
 			returned_data = data_gen.get_data(data)
-			gear = returned_data['Gear']
-			rpm = round(returned_data['CurrentEngineRpm'])
-			power = round((returned_data['Power']*1.34102)/1000)
-			torque = round(returned_data['Torque'], 1) 
-			boost = round((returned_data['Boost']/14.504), 2)
-			#print(gear, gear_setting)
+			gear = returned_data["Gear"]
+			rpm = round(returned_data["CurrentEngineRpm"])
+			power = round((returned_data["Power"] * 1.34102) / 1000)
+			torque = round(returned_data["Torque"], 1)
+			boost = round((returned_data["Boost"] / 14.504), 2)
+			# print(gear, gear_setting)
 			if gear_setting == gear:
 				break
 			else:
@@ -97,22 +114,22 @@ def graph():
 								rpm_axis.append(rpm)
 								power_axis.append(power)
 								torque_axis.append(torque)
-								boost_axis.append(boost*10)
+								boost_axis.append(boost * 10)
 							else:
 								continue
 						else:
 							rpm_axis.append(rpm)
 							power_axis.append(power)
 							torque_axis.append(torque)
-							boost_axis.append(boost*10)
+							boost_axis.append(boost * 10)
 					else:
 						continue
 				else:
 					continue
-		#print(rpm_axis)
-		#print(power_axis)
-		#print(torque_axis)
-		#print(boost_axis)
+		print(rpm_axis)
+		print(power_axis)
+		print(torque_axis)
+		print(boost_axis)
 		dpg.set_value("graph_status", "Peak ploting")
 		peak_hp = max(power_axis)
 		peak_hp_index = power_axis.index(max(power_axis))
@@ -123,15 +140,15 @@ def graph():
 		peak_boost = round(max(boost_axis), 2)
 		peak_boost_index = boost_axis.index(max(boost_axis))
 		peak_boost_rpm = rpm_axis[peak_boost_index]
-		#print(peak_hp, peak_hp_rpm)
-		#print(peak_torque, peak_torque_rpm)
-		#print(peak_boost, peak_boost_rpm)
+		# print(peak_hp, peak_hp_rpm)
+		# print(peak_torque, peak_torque_rpm)
+		# print(peak_boost, peak_boost_rpm)
 		dpg.set_value("graph_status", "Graphing")
 		fig, ax = plt.subplots(figsize=(6, 6))
 		ax.plot(x, y, label="Power HP")
 		ax.plot(x, z, label="Torque N.m")
 		ax.plot(x, az, label="boost bar*10")
-		ax.plot(peak_hp_rpm, peak_hp, marker="o", markersize=5, markeredgecolor="red", markerfacecolor="green")
+		ax.plot(peak_hp_rpm, peak_hp, marker="o", markersize=5, markeredgecolor="red", markerfacecolor="green")      
 		ax.plot(peak_torque_rpm, peak_torque, marker="o", markersize=5, markeredgecolor="red", markerfacecolor="green")
 		ax.plot(peak_boost_rpm, peak_boost, marker="o", markersize=5, markeredgecolor="red", markerfacecolor="green")
 		ax.legend()
@@ -141,12 +158,12 @@ def graph():
 	except:
 		dpg.set_value("graph_status", "Error")
 
-with dpg.window(label="Main", autosize=True, pos=(10,10)):
+with dpg.window(label="Main", autosize=True, pos=(10, 10)):
 	dpg.add_input_text(label="IP", source="ip_address")
 	dpg.add_input_text(label="PORT", source="port")
 	dpg.add_button(label="Connect", callback=connect)
 
-with dpg.window(label="Stats", autosize=True, pos=(10,175)):
+with dpg.window(label="Stats", autosize=True, pos=(10, 175)):
 	dpg.add_button(label="run", callback=run)
 	dpg.add_text(source="gear")
 	dpg.add_text(source="rpm")
@@ -154,12 +171,12 @@ with dpg.window(label="Stats", autosize=True, pos=(10,175)):
 	dpg.add_text(source="torque")
 	dpg.add_text(source="boost")
 
-with dpg.window(label="Graph", autosize=True, pos=(125,175)):
+with dpg.window(label="Graph", autosize=True, pos=(125, 175)):
 	dpg.add_slider_int(label="Gearbox", min_value=1, max_value=10, source="gearbox", width=50)
 	dpg.add_text(source="gear_setting")
 	dpg.add_button(label="graph", callback=graph)
 
-with dpg.window(label="Status", autosize=True, pos=(400,175)):
+with dpg.window(label="Status", autosize=True, pos=(400, 175)):
 	dpg.add_text(default_value="Connection status :")
 	dpg.add_text(source="connect_status")
 	dpg.add_text(default_value="Run status :")
@@ -167,7 +184,10 @@ with dpg.window(label="Status", autosize=True, pos=(400,175)):
 	dpg.add_text(default_value="Graph status :")
 	dpg.add_text(source="graph_status")
 
-dpg.create_viewport(title='ForzaTelemetryApp', width=600, height=400, always_on_top=True)
+with dpg.window(label="Graph Status", autosize=True, pos=(400, 10)):
+	dpg.add_text(default_value="Graph status")
+
+dpg.create_viewport(title="ForzaTelemetryApp", width=600, height=400, always_on_top=True)
 dpg.setup_dearpygui()
 dpg.show_viewport()
 dpg.start_dearpygui()
