158e1bb2837fe51be7b7aac989b3dbf1398f26e7
Fauwzk <polentesaxel@gmail.com>
2022-04-17 02:09:58 +0200

    Modified default layout and add to export_exe

--- .vscode/export_exe.cmd
+++ .vscode/export_exe.cmd
@@ -1,4 +1,5 @@
 @echo off
+rmdir /s /q dist
 rmdir /s /q output
 pyinstaller --noconfirm --onefile --console --name "ForzaTelemetryApp" --add-data "data_format.txt;." --add-data "font.ttf;." --add-data "data_gen.py;." --add-data "fta.ini;." "main.py"
 ren dist output
