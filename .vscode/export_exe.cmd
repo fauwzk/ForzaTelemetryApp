80d6a0ec0f0d90aafa0fd473c3e612aff87ed5cd
unknown <polentesaxel@gmail.com>
2022-04-30 21:54:59 +0200

    Removed code formater

--- .vscode/export_exe.cmd
+++ .vscode/export_exe.cmd
@@ -1,5 +1,4 @@
 @echo off
 rmdir /s /q dist
-python -m black src\*.py
 pyinstaller --noconfirm --onefile --console --name "ForzaTelemetryApp" --add-data "src\data_format.txt;." --add-data "src\font.otf;." --add-data "src\data_gen.py;." --add-data "src\default.ini;." "src\main.py"
 rmdir /s /q build
--- src/main.py
+++ src/main.py
@@ -17,14 +17,11 @@ from rich.text import Text
 from pathlib import Path, PureWindowsPath
 
 # Need rewriting with threading
-warnings.filterwarnings(
-    "ignore",
-    message="Starting a Matplotlib GUI outside of the main thread will likely fail.",
-)
+warnings.filterwarnings("ignore", message="Starting a Matplotlib GUI outside of the main thread will likely fail.")
 
 dpg.create_context()
 
-default_initfile = PureWindowsPath("src/default.ini")
+default_initfile = "src\\default.ini"
 initfile = "fta.ini"
 global home_path
 home_path = os.path.expanduser("~")
@@ -60,7 +57,6 @@ with dpg.font_registry():
 
 dpg.bind_font(app_font)
 
-
 def connect():
     global connection_status
     ip = dpg.get_value("ip_address")
@@ -69,9 +65,7 @@ def connect():
         dpg.set_value("status", f"Connecting to {ip}:{port}")
         dpg.configure_item("status_window", show=True)
         data_gen.set_server(ip, port)
-        data, addr = data_gen.sock.recvfrom(
-            1500
-        )  # buffer size is 1500 bytes, this line reads data from the socket
+        data, addr = data_gen.sock.recvfrom(1500)  # buffer size is 1500 bytes, this line reads data from the socket
         returned_data = data_gen.get_data(data)
         dpg.set_value("status", "Connected")
         # dpg.configure_item("status_window", show=False)
@@ -87,14 +81,12 @@ def connect():
         dpg.set_value("status", f"Error:\n{e}")
         dpg.configure_item("status_window", show=True)
 
-
 def save_init_file():
-    init_file = PureWindowsPath(f"{home_path}/{initfile}")
+    init_file = f"{home_path}\\{initfile}"
     dpg.save_init_file(init_file)
     dpg.configure_item("save_window", show=False)
     dpg.set_value("save_status", f"Saved:\n{init_file}")
     dpg.configure_item("save_window", show=True)
-
 
 def graph():
     if connection_status != 0:
@@ -162,7 +154,6 @@ def graph():
         dpg.set_value("error", "Not connected")
         dpg.configure_item("error_window", show=True)
 
-
 def open_values(sender, app_data, user_data):
     rpm_axis = []
     power_axis = []
@@ -187,7 +178,6 @@ def open_values(sender, app_data, user_data):
         boost_axis = data["boost"]
     dpg.set_value("run_status", "Peak ploting")
     make_graph(rpm_axis, power_axis, torque_axis, boost_axis)
-
 
 def save_values():
     if connection_status != 0:
@@ -215,7 +205,6 @@ def save_values():
         dpg.set_value("error", "Not connected")
         dpg.configure_item("error_window", show=True)
 
-
 def make_graph(rpm_axis, power_axis, torque_axis, boost_axis):
     peak_hp = max(power_axis)
     peak_hp_index = power_axis.index(max(power_axis))
@@ -234,22 +223,10 @@ def make_graph(rpm_axis, power_axis, torque_axis, boost_axis):
     markersize_setting = 15
     dpg.set_value("run_status", "Graphing")
     fig, ax = plt.subplots(figsize=(6, 6))
-    ax.plot(
-        rpm_axis,
-        power_axis,
-        label=f"Power HP\n{peak_hp}HP@{peak_hp_rpm}RPM",
-    )
-    ax.plot(
-        rpm_axis,
-        torque_axis,
-        label=f"Torque N.m\n{peak_torque}N.m@{peak_torque_rpm}RPM",
-    )
+    ax.plot(rpm_axis, power_axis, label=f"Power HP\n{peak_hp}HP@{peak_hp_rpm}RPM")
+    ax.plot(rpm_axis, torque_axis, label=f"Torque N.m\n{peak_torque}N.m@{peak_torque_rpm}RPM")
     if peak_boost != 0:
-        ax.plot(
-            rpm_axis,
-            boost_axis,
-            label=f"boost bar*10\n{round(peak_boost/10, 2)}bar@{peak_boost_rpm}RPM",
-        )
+        ax.plot(rpm_axis, boost_axis, label=f"boost bar*10\n{round(peak_boost/10, 2)}bar@{peak_boost_rpm}RPM")
         ax.plot(peak_boost_rpm, peak_boost, marker="|", markersize=markersize_setting)
     ax.plot(peak_hp_rpm, peak_hp, marker="|", markersize=markersize_setting)
     ax.plot(peak_torque_rpm, peak_torque, marker="|", markersize=markersize_setting)
@@ -262,14 +239,11 @@ def make_graph(rpm_axis, power_axis, torque_axis, boost_axis):
     del torque_axis[:]
     del boost_axis[:]
 
-
 def get_telemetry():
     global connection_status
     if connection_status == 1:
         try:
-            data, addr = data_gen.sock.recvfrom(
-                1500
-            )  # buffer size is 1500 bytes, this line reads data from the socket
+            data, addr = data_gen.sock.recvfrom(1500)  # buffer size is 1500 bytes, this line reads data from the socket
             returned_data = data_gen.get_data(data)
             gear = returned_data["Gear"]
             rpm = round(returned_data["CurrentEngineRpm"])
@@ -293,15 +267,7 @@ def get_telemetry():
             dpg.configure_item("error_window", show=True)
             connection_status = 0
 
-with dpg.file_dialog(
-    modal=True,
-    show=False,
-    callback=open_values,
-    id="open_values_file_picker",
-    width=500,
-    height=400,
-    default_path=home_path,
-):
+with dpg.file_dialog(modal=True, show=False, callback=open_values, id="open_values_file_picker", width=500, height=400, default_path=home_path):
     dpg.add_file_extension(".json", color=(255, 0, 255, 255), custom_text="[JSON]")
 
 with dpg.window(label="Main", autosize=True, pos=(10, 30)):
@@ -340,85 +306,44 @@ with dpg.window(label="Car Telemetry", autosize=True, pos=(10, 175)):
     dpg.bind_item_font(t6, telemetry_font)
 
 with dpg.window(label="Graph", autosize=True, pos=(125, 175)):
-    dpg.add_slider_int(
-        label="Gearbox", min_value=2, max_value=10, source="gearbox", width=62
-    )
+    dpg.add_slider_int(label="Gearbox", min_value=2, max_value=10, source="gearbox", width=62)
     dpg.add_button(label="Start", callback=graph)
 
-with dpg.window(
-    label="App error",
-    autosize=True,
-    modal=True,
-    show=False,
-    id="error_window",
-    no_title_bar=True,
-):
+with dpg.window(label="App error",autosize=True,modal=True,show=False,id="error_window",no_title_bar=True):
     dpg.add_text(default_value="Error:")
     dpg.add_text(source="error")
-    dpg.add_button(
-        label="Close",
-        width=75,
-        callback=lambda: dpg.configure_item("error_window", show=False),
-    )
+    dpg.add_button(label="Close", width=75, callback=lambda: dpg.configure_item("error_window", show=False))
 
 with dpg.window(label="App Status", autosize=True):
     dpg.add_text(default_value="Status :")
     dpg.add_text(source="run_status")
 
-with dpg.window(
-    label="Status",
-    autosize=True,
-    modal=True,
-    show=False,
-    id="status_window",
-    no_title_bar=True,
-):
+with dpg.window(label="Status",autosize=True,modal=True,show=False,id="status_window",no_title_bar=True):
     dpg.add_text(source="status")
-    dpg.add_button(
-        label="Close",
-        width=75,
-        callback=lambda: dpg.configure_item("status_window", show=False),
-    )
+    dpg.add_button(label="Close", width=75, callback=lambda: dpg.configure_item("status_window", show=False))
 
 with dpg.viewport_menu_bar():
     with dpg.menu(label="Logs"):
-        dpg.add_menu_item(
-            label="Open", callback=lambda: dpg.show_item("open_values_file_picker")
-        )
+        dpg.add_menu_item(label="Open", callback=lambda: dpg.show_item("open_values_file_picker"))
         dpg.add_menu_item(label="Save", callback=save_values)
     with dpg.menu(label="Config"):
         dpg.add_menu_item(label="Save", callback=save_init_file)
     with dpg.menu(label="Debug"):
         dpg.add_menu_item(label="Metrics", callback=lambda: dpg.show_metrics())
 
-with dpg.window(
-    label="Save Status",
-    autosize=True,
-    modal=True,
-    show=False,
-    id="save_window",
-    no_title_bar=True,
-):
+with dpg.window(label="Save Status", autosize=True, modal=True, show=False, id="save_window", no_title_bar=True):
     dpg.add_text(source="save_status")
-    dpg.add_button(
-        label="Close",
-        width=75,
-        callback=lambda: dpg.configure_item("save_window", show=False),
-    )
+    dpg.add_button(label="Close", width=75, callback=lambda: dpg.configure_item("save_window", show=False))
 
 with dpg.theme() as global_theme:
     with dpg.theme_component(dpg.mvAll):
-        dpg.add_theme_style(
-            dpg.mvStyleVar_FrameRounding, 5, category=dpg.mvThemeCat_Core
-        )
-        dpg.add_theme_style(
-            dpg.mvStyleVar_GrabRounding, 5, category=dpg.mvThemeCat_Core
-        )
+        dpg.add_theme_style(dpg.mvStyleVar_FrameRounding, 5, category=dpg.mvThemeCat_Core)
+        dpg.add_theme_style(dpg.mvStyleVar_GrabRounding, 5, category=dpg.mvThemeCat_Core)
 
 dpg.bind_theme(global_theme)
 console = Console()
 
-home_init_file = PureWindowsPath(f"{home_path}/{initfile}")
+home_init_file = f"{home_path}\\{initfile}"
 init_exist = os.path.exists(home_init_file)
 if init_exist == False:
     text = Text("Copying defaut init file to home folder")
