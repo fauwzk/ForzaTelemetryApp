467e145d33f1bcd9ddfa94622488a416dabcf44c
Axel Polentes <polentesaxel@gmail.com>
2022-08-09 22:54:28 +0200

    Update data_gen.py

--- data_gen.py
+++ data_gen.py
@@ -2,7 +2,7 @@ import struct
 import socket
 import sys
 import os
-
+from pathlib import Path
 
 def set_server(ip, port):
     """
@@ -38,7 +38,7 @@ def resource_path(relative_path):
 # reading data and assigning names to ata types in data_types dict
 # Reading the data_format.txt file and creating a dictionary with the data types.
 data_types = {}
-with open(resource_path("src\data_format.txt"), "r") as f:
+with open(resource_path(Path("src/data_format.txt")), "r") as f:
     lines = f.read().split("\n")
     for line in lines:
         data_types[line.split()[1]] = line.split()[0]
